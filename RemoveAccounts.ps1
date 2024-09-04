fd98771c785f72dc7566e0b464c979bab3508639
Mark <148797232+MarkCyber@users.noreply.github.com>
2024-09-04 14:45:33 -0400

    Update RemoveAccounts.ps1

--- RemoveAccounts.ps1
+++ RemoveAccounts.ps1
@@ -1,27 +1,13 @@
-# THIS IS A WORK IN PROGRESS
-
-# Define the usernames to be removed
-$usernames = @("John.Test", "Marcus.Bonley", "Test.acc")
- 
-# Get the list of currently logged on users
+$usernames = @("John.doesNotexistttt", "selci.tset", "testingthis")
 $loggedOnUsers = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
- 
-# Loop through each username
 foreach ($username in $usernames) {
-    # Find the user's SID
+    # Find SID
     $sid = (New-Object System.Security.Principal.NTAccount($username)).Translate([System.Security.Principal.SecurityIdentifier]).Value
- 
-    # Check if the user is currently logged in
     if ($loggedOnUsers -contains $username) {
-        Write-Output "$username is currently logged on. Logging them off..."
- 
-        # Log off the user
         logoff (quser | Select-String $username | ForEach-Object { $_.Line.Split()[2] }) /server:localhost
- 
-        Start-Sleep -Seconds 5  # Wait for a few seconds to ensure the user is logged off
+        Start-Sleep -Seconds 5  # Wait for a few seconds to ensure a successful logoff
     }
- 
-    # Remove the user's profile
+    #Remove profile
     try {
         Write-Output "Removing user profile for $username with SID $sid..."
         $profilePath = Get-WmiObject -Class Win32_UserProfile | Where-Object { $_.SID -eq $sid } | Select-Object -ExpandProperty LocalPath
