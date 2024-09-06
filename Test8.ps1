26c70990784561bfa0bbf827c8f0369ea6bb621c
Mark <148797232+MarkCyber@users.noreply.github.com>
2024-09-06 09:28:34 -0400

    Update and rename Test 8.ps1 to Test8.ps1

--- Test8.ps1
+++ Test8.ps1
@@ -1,4 +1,4 @@
-# THIS 
+
 
 $usernames = @("John.doesNotexistttt", "Test.Account", "testingthis")
 $loggedOnUsers = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
